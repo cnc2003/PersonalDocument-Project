d9e20308dae42c98adb2e13ee1fc6c3632e7b1c4
cnc2003 <111475386+cnc2003@users.noreply.github.com>
2024-10-20 00:05:42 +0700

    update backend cross-origins

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -3,11 +3,24 @@ package routes
 import (
 	"document-backend/controllers"
 	"document-backend/middleware"
+	"time"
+
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRouter() *gin.Engine {
 	router := gin.Default()
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost"}, // Allow React frontend origin
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+		AllowHeaders:     []string{"Authorization", "Content-Type"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
 
 	router.POST("/register", controllers.Register)
 	router.POST("/login", controllers.Login)
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Link } from 'react-router-dom'
-
-
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const login = async () => {
+    try {
+      const response = await axios.post(
+        "http://localhost:8080/login",
+        {
+          user: "alice@example.com",
+          password: "password123",
+        },
+        {
+          withCredentials: true, // Include credentials (cookies, etc.)
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      console.log("Login response:", response.data);
+    } catch (error) {
+      console.error("Error during login:", error);
+    }
+  }
 
+  login();
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-      Hello Document miner!!!
-    </h1>
-    <Link to={`signin`} className='btn'> singin </Link>
+      <h1 className="text-3xl font-bold underline">Hello Document miner!!!</h1>
+      <Link to={`signin`} className="btn">
+        {" "}
+        singin{" "}
+      </Link>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
