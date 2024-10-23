5a448f081c6faf7a5ca176e3d6c2c131ff347a86
cnc2003 <111475386+cnc2003@users.noreply.github.com>
2024-10-23 14:57:53 +0700

    upd:add field emoji

--- backend/models/document.go
+++ backend/models/document.go
@@ -8,6 +8,7 @@ type Document struct {
 	ID        int       `gorm:"primaryKey" json:"id"`
 	UserID    int       `gorm:"not null" json:"user_id"`
 	Title     string    `gorm:"not null" json:"title"`
+	Emoji     string    `gorm:"column:emoji;type:test" json:"emoji"`
 	ImageUrl  string    `gorm:"column:imageUrl;type:text" json:"imageUrl"`
 	Content   string    `json:"content"`
 	CreatedOn time.Time `gorm:"autoCreateTime;column:createdOn" json:"createdOn"`
