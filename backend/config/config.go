ee9209dec3b57e060c34a161626d7c77f83ab4ab
cnc2003 <111475386+cnc2003@users.noreply.github.com>
2024-11-10 12:25:16 +0700

    fix: Refactor database connection to use MYSQL_URL environment variable and remove commented code

--- backend/config/config.go
+++ backend/config/config.go
@@ -1,7 +1,7 @@
 package config
 
 import (
-	"fmt"
+	// "fmt"
 	"log"
 	"os"
 
@@ -20,18 +20,19 @@ func ConnectDatabase() {
 	}
 
 	// Retrieve the environment variables
-	host := os.Getenv("MYSQLHOST")
-	port := os.Getenv("MYSQLPORT")
-	user := os.Getenv("MYSQLUSER")
-	password := os.Getenv("MYSQLPASSWORD")
-	dbname := os.Getenv("MYSQLDATABASE")
+	// host := os.Getenv("MYSQLHOST")
+	// port := os.Getenv("MYSQLPORT")
+	// user := os.Getenv("MYSQLUSER")
+	// password := os.Getenv("MYSQLPASSWORD")
+	// dbname := os.Getenv("MYSQLDATABASE")
 
 	// Log the DSN string (for debugging only, be careful with sensitive info)
 	// dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
 	// 	user, password, host, port, dbname)
 	// fmt.Println("Connecting with DSN:", dsn)
 
-	url := fmt.Sprintf("mysql://%s:%s@%s:%s/%s", user, password, host, port, dbname)
+	// url := fmt.Sprintf("mysql://%s:%s@%s:%s/%s", user, password, host, port, dbname)
+	url := os.Getenv("MYSQL_URL")
 	// Try to connect to the database
 	var err error
 	DB, err = gorm.Open(mysql.Open(url), &gorm.Config{})
